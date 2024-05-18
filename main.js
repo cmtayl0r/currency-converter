0912a3a1d203eb11baf43e5c476d02d75e486da5
Chris Taylor <134958907+cmtayl0r@users.noreply.github.com>
2024-05-18 22:23:02 +0200

    add toggle drawer open and id state

--- main.js
+++ main.js
@@ -2,17 +2,49 @@ import axios from 'axios';
 
 // fca_live_9yWcaJ8XzcMUnwWXMLF0GHVLBVAnl3ROKuSUdu4Q
 
+const state = {
+    openedDrawer: null,
+};
+
 // -----------------------------------------------------------------------------
 // DOM SELECTORS
 // -----------------------------------------------------------------------------
+
+const ui = {
+    controls: document.getElementById('controls'),
+    drawer: document.getElementById('drawer'),
+    dismissBtn: document.getElementById('dismiss-btn'),
+};
 
 // -----------------------------------------------------------------------------
 // EVENT LISTENERS
 // -----------------------------------------------------------------------------
 
+const setupEventListeners = () => {
+    ui.controls.addEventListener('click', showDrawer);
+    ui.dismissBtn.addEventListener('click', hideDrawer);
+};
+
 // -----------------------------------------------------------------------------
 // EVENT HANDLERS
 // -----------------------------------------------------------------------------
+const showDrawer = event => {
+    // Checks if the clicked element has the data-drawer attribute
+    // isolates the button inside the controls as the clickable elements
+    if (event.target.hasAttribute('data-drawer')) {
+        // Sets the openedDrawer state to the id of the clicked button
+        // This is either 'base' or 'target'
+        state.openedDrawer = event.target.id;
+        console.log(state.openedDrawer);
+        ui.drawer.classList.add('show');
+    }
+};
+const hideDrawer = event => {
+    // Resets the openedDrawer state to null
+    state.openedDrawer = null;
+    ui.drawer.classList.remove('show');
+    console.log(state.openedDrawer);
+};
 
 // -----------------------------------------------------------------------------
 // RENDER FUNCTIONS
@@ -30,4 +62,6 @@ import axios from 'axios';
 // INITIALIZATION
 // -----------------------------------------------------------------------------
 
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+    setupEventListeners();
+});
