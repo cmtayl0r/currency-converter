fcdb8ba66f41a60f23c868c26b795c0e1e5040a5
Chris Taylor <134958907+cmtayl0r@users.noreply.github.com>
2024-05-18 23:53:20 +0200

    add async fetch api data function and render currencies list

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
-// fca_live_9yWcaJ8XzcMUnwWXMLF0GHVLBVAnl3ROKuSUdu4Q
+// TODO: Correct keyboard focus, and focus trap
+
+// -----------------------------------------------------------------------------
+// STATE
+// -----------------------------------------------------------------------------
+const API_KEY = 'fca_live_9yWcaJ8XzcMUnwWXMLF0GHVLBVAnl3ROKuSUdu4Q';
+// const API_LATEST = 'https://api.freecurrencyapi.com/v1/latest';
+const API_CURRENCIES = `https://api.freecurrencyapi.com/v1/currencies?apikey=`;
 
 const state = {
     openedDrawer: null,
+    currencies: [],
 };
 
 // -----------------------------------------------------------------------------
@@ -14,6 +22,7 @@ const ui = {
     controls: document.getElementById('controls'),
     drawer: document.getElementById('drawer'),
     dismissBtn: document.getElementById('dismiss-btn'),
+    currencyList: document.getElementById('currency-list'),
 };
 
 // -----------------------------------------------------------------------------
@@ -21,6 +30,7 @@ const ui = {
 // -----------------------------------------------------------------------------
 
 const setupEventListeners = () => {
+    document.addEventListener('DOMContentLoaded', initApp);
     ui.controls.addEventListener('click', showDrawer);
     ui.dismissBtn.addEventListener('click', hideDrawer);
 };
@@ -28,6 +38,9 @@ const setupEventListeners = () => {
 // -----------------------------------------------------------------------------
 // EVENT HANDLERS
 // -----------------------------------------------------------------------------
+
+const initApp = () => {};
+
 const showDrawer = event => {
     // Checks if the clicked element has the data-drawer attribute
     // isolates the button inside the controls as the clickable elements
@@ -35,7 +48,6 @@ const showDrawer = event => {
         // Sets the openedDrawer state to the id of the clicked button
         // This is either 'base' or 'target'
         state.openedDrawer = event.target.id;
-        console.log(state.openedDrawer);
         ui.drawer.classList.add('show');
     }
 };
@@ -43,25 +55,70 @@ const hideDrawer = event => {
     // Resets the openedDrawer state to null
     state.openedDrawer = null;
     ui.drawer.classList.remove('show');
-    console.log(state.openedDrawer);
 };
 
 // -----------------------------------------------------------------------------
 // RENDER FUNCTIONS
 // -----------------------------------------------------------------------------
 
+const renderCurrencies = () => {
+    // map over the currencies array and create a list item for each currency
+    // destructuring the code and name from each currency object
+    ui.currencyList.innerHTML = state.currencies
+        .map(({ code, name }) => {
+            return `
+            <li data-code="${code}">
+                <img src="${getImageURL(code)}" alt="${name}" />
+                <div>
+                    <h4>${code}</h4>
+                    <p>${name}</p>
+                </div>
+            </li>`;
+        })
+        .join('');
+};
+
 // -----------------------------------------------------------------------------
 // HELPER FUNCTIONS
 // -----------------------------------------------------------------------------
+
+const getImageURL = code => {
+    const flag =
+        'https://wise.com/public-resources/assets/flags/rectangle/{code}.png';
+    // replace the {code} placeholder in the URL with the lowercase currency code
+    return flag.replace('{code}', code.toLowerCase());
+};
 
 // -----------------------------------------------------------------------------
 // API FUNCTIONS
 // -----------------------------------------------------------------------------
 
+const fetchCurrencies = async () => {
+    try {
+        // 1 - Wait for fetch call (which is a promise)
+        let response = await fetch(`${API_CURRENCIES}${API_KEY}`);
+        // 2 - Check for HTTP errors
+        if (!response.ok)
+            throw new Error(`HTTP error! status: ${response.status}`);
+        // 3 - Wait for the conversion of the response to JSON
+        // and destructure the data object from the response
+        const { data } = await response.json();
+        // 4 - Do something with the data
+        // add the values of the data object to the currencies array in the state
+        state.currencies = Object.values(data);
+        console.log(state.currencies);
+        renderCurrencies();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// https://wise.com/public-resources/assets/flags/rectangle/{code}.png
 // -----------------------------------------------------------------------------
 // INITIALIZATION
 // -----------------------------------------------------------------------------
 
 document.addEventListener('DOMContentLoaded', () => {
     setupEventListeners();
+    fetchCurrencies();
 });
